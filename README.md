b4c04119a6338974eb95ebd9b76cf38f9ee2c1a0
swatitaneja <swatitaneja404@gmail.com>
2020-06-11 15:12:15 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# 247-schema
+Single place which has all the schema for 247 organisation. 
---
+++ index.js
@@ -0,0 +1,97 @@
+import branding from './theme/branding';
+import color from './theme/color';
+import font from './theme/font';
+import icons from './theme/icons';
+import language from './theme/language';
+import size from './theme/size';
+import promo from './theme/promo';
+import legalese from './theme/legalese';
+import notifications from './theme/notifications';
+import oop from './theme/oop';
+import postchatControls from './theme/postchatControls';
+import fileupload from './theme/fileupload';
+import cobrowse from './theme/cobrowse';
+import supportHours from './theme/supportHours';
+import api from './advanced/api';
+import create from './advanced/channels/new';
+import channels from './advanced/channels/edit';
+import clone from './advanced/channels/clone';
+import envDetails from './advanced/envDetails';
+import messages from './advanced/messages';
+import security from './advanced/security';
+import widget from './advanced/widget';
+import genericGlobal from './invites/generic-global';
+import generic from './invites/generic';
+import direct from './invites/direct';
+import clientManaged from './invites/client-managed';
+import clientManagedGlobal from './invites/client-managed-global';
+import addInvite from './invites/addInvite';
+import container from './invites/container';
+import decorative from './invites/decorative';
+import action from './invites/action';
+import context from './invites/context';
+import customInvite from './invites/custom';
+import waiting from './engagements/waiting';
+import offline from './engagements/offline';
+import busy from './engagements/busy';
+import onboard from './onboard';
+import rollback from './rollback';
+import compare from './compare';
+import accountsettings from './account-settings';
+import i18nCustom from './theme/i18n/custom';
+import appleChannel from './channels/apple';
+import facebookChannel from './channels/facebook';
+import googleBusinessChannel from './channels/googleBusiness';
+import googleRCSChannel from './channels/googleRCS';
+import smsChannel from './channels/sms';
+import commonChannel from './channels/common';
+
+export default {
+  branding,
+  color,
+  create,
+  font,
+  icons,
+  language,
+  size,
+  promo,
+  legalese,
+  notifications,
+  oop,
+  postchatControls,
+  fileupload,
+  cobrowse,
+  supportHours,
+  api,
+  channels,
+  clone,
+  envDetails,
+  messages,
+  security,
+  widget,
+  genericGlobal,
+  generic,
+  direct,
+  clientManaged,
+  clientManagedGlobal,
+  addInvite,
+  container,
+  decorative,
+  action,
+  context,
+  customInvite,
+  waiting,
+  offline,
+  busy,
+  onboard,
+  rollback,
+  compare,
+  i18nCustom,
+  accountsettings,
+  appleChannel,
+  facebookChannel,
+  googleBusinessChannel,
+  googleRCSChannel,
+  smsChannel,
+  commonChannel
+};
